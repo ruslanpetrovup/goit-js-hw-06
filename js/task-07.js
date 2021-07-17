9d82d95de14d2d7bfa50d2506e83cb7cf95db460
Ruslan Petrov <48686933+ruslanpetrov228@users.noreply.github.com>
2021-07-17 19:53:07 +0300

    correct

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector('#font-size-control');
 let spanEl = document.querySelector('#text');
-
+spanEl.style.fontSize = '0px'
 const editValue = () => {
  spanEl.style.fontSize = `${inputEl.value}px`;   
 }
